546eae99f9b5cce8ff30179a3f4854e5a95b1934
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-07-07 21:53:35 -0700

    added styles to feed

--- boldstatements_django/boldstatements/models.py
+++ boldstatements_django/boldstatements/models.py
@@ -5,7 +5,7 @@ class Statement(models.Model):
     prediction = models.CharField(max_length=500)
     predictor = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='documents/', blank=True)
+    image = models.ImageField(blank=True, upload_to='documents/')
     link = models.TextField(blank=True)
     datestamp = models.DateTimeField(auto_now_add=True)
 
