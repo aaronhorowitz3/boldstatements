5272c067bd835c6d0ce6fe894f221806f01cdcb8
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-07-10 18:49:51 -0700

    found a sort solution, sort of. added fa icons to google and github auth

--- boldstatements_django/boldstatements/static/scripts/app.js
+++ boldstatements_django/boldstatements/static/scripts/app.js
@@ -1,66 +1,20 @@
 $(document).ready(function() {
    console.log('faaaack')
 
-   $.sortByDate = function(elements, order) {
-      var arr = [];
-      elements.each(function() {
-         var obj = {},
-            $el = $(this),
-            time = $el.find(".prediction-date").text(),
-            date = new Date($.trim(time)),
-            timestamp = date.getTime();
-            obj.html = $el[0].outerHTML;
-            obj.time = timestamp;
-            arr.push(obj);
-      });
+   let newestFirst = $('#sorted-feed')
+   let oldestFirst = $('#reverse-sorted-feed')
+   let newer = $('#newer')
+   let older = $('#older')
 
-      var sorted = arr.sort(function(a,b) {
-         if(order == "ASC") {
-            return a.time > b.time;
-         } else {
-            return b.time > a.time;
-         }
-      });
-      return sorted;
-   };
+   older.click(function() {
+      newestFirst.addClass('hidden')
+      oldestFirst.removeClass('hidden')
+   })
 
-   $(function() {
-		var $newer = $("#newer"),
-			$older = $("#older"),
-			$content = $("#sorted-feed"),
-			$elements = $(".in-sort-order");
-
-			$newer.click(function() {
-				var elements = $.sortByDate($elements, "DESC");
-				var html = "";
-				for(var i = 0; i < elements.length; ++i) {
-					html += elements[i].html;
-				}
-				$content[0].innerHTML = html;
-				$(this).addClass("selected").
-				siblings().
-				removeClass("selected");
-				return false;
-
-			});
-
-			$older.click(function() {
-				var elements = $.sortByDate( $elements, "ASC" );
-				var html = "";
-				for( var i = 0; i < elements.length; ++i ) {
-					html += elements[i].html;
-				}
-				$content[0].innerHTML = html;
-				$( this ).addClass( "selected" ).
-				siblings().
-				removeClass( "selected" );
-				return false;
-
-			});
-	});
-
-
-
+   newer.click(function() {
+      newestFirst.removeClass('hidden')
+      oldestFirst.addClass('hidden')
+   })
 
    let statementContainer = $('#single-statement')
    let trueRadio = $('#true-radio')
